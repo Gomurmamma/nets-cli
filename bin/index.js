b027d592fafcba445c31576656e1d775ec205083
Gomurmamma <dnaldyoung@gmail.com>
2022-11-08 12:23:08 -0500

    Defining game-today command. WIP.

--- bin/index.js
+++ bin/index.js
@@ -1,2 +1,9 @@
 #! /usr/bin/env node
 console.log("Hello world!");
+
+const { program } = require("commander");
+
+program
+  .command("game-today")
+  .description("Lists all Brooklyn Nets NBA games for today")
+  .action(game - today);
